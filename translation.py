9bfec35186145fe7c0aa1bc82f73c24ae5f16a59
YunxiaoWangwww <47125773+YunxiaoWangwww@users.noreply.github.com>
2021-03-28 19:55:44 +0100

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,37 @@
+#amino_acid translation
+def translate(seq):
+    DNAtoProtein_table = {
+            'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+            'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+            'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+            'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+            'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+            'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+            'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+            'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+            'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+            'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+            'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+            'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+            'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+            'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+            'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+            'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W'}
+    f = lambda seq, n=3: [seq[i:i + 3] for i in range(0, len(seq), 3)]
+    mylist = []
+    for codes in f(seq):
+            code = DNAtoProtein_table.get(codes)
+            mylist.append(code)
+    my_list = ''.join(mylist)
+    return my_list
+    
+
+#reverse transcription
+def mRNA_to_gene(seq):
+    seq = list(seq)
+    for i in range(len(seq)):
+        if seq[i] == 'U':
+            seq[i] = 'T'
+    return "".join(seq)
+dna = mRNA_to_gene("CCCCGUACGGAGAUGAAA")
+print(translate(dna))
