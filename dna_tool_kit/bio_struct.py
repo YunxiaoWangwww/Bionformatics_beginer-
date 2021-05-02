70a196f65822eaf0cc974ed38efdc870dfbf51df
YunxiaoWangwww <47125773+YunxiaoWangwww@users.noreply.github.com>
2021-05-02 14:12:01 +0100

    Create bio_struct.py

---
+++ dna_tool_kit/bio_struct.py
@@ -0,0 +1,39 @@
+NUCLEOTIDE_BASE = {
+    "DNA" : ["A","T","C","G"],
+    "RNA" : ["A","U","C","G"]
+}
+
+DNAtoProtein_table = {
+            'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+            'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+            'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+            'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+            'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+            'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+            'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+            'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+            'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+            'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+            'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+            'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+            'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+            'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+            'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+            'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W'}
+RNAtoProtein_table = {
+            "UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+            "UCU":"S", "UCC":"s", "UCA":"S", "UCG":"S",
+            "UAU":"Y", "UAC":"Y", "UAA":"_", "UAG":"_",
+            "UGU":"C", "UGC":"C", "UGA":"_", "UGG":"W",
+            "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+            "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+            "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+            "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+            "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+            "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+            "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+            "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+            "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+            "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+            "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+            "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"}
